9210504b6d88e373dd2c90fd6b82d6d6973a7b9c
Christoph Reiter <reiter.christoph@gmail.com>
2020-10-10 20:17:50 +0200

    wip

---
+++ mirrorbits/add_mirrors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mirrorbits add -http=https://mirror.yandex.ru/mirrors/msys2/ -rsync=rsync://mirror.yandex.ru/mirrors/msys2/ -score 1000 yandex.ru
+mirrorbits enable yandex.ru
+
+mirrorbits add -http=https://repo.msys2.org/ -rsync=rsync://repo.msys2.org/builds/ -score 1 msys2.org
+mirrorbits enable msys2.org
+
+mirrorbits add -http=https://mirror.selfnet.de/msys2/ -rsync=rsync://mirror.selfnet.de/msys2/ -score 1000 selfnet.de
+mirrorbits enable selfnet.de
+
+mirrorbits add -http=https://mirrors.tuna.tsinghua.edu.cn/msys2/ -rsync=rsync://mirror.selfnet.de/msys2/ -score 1000 tsinghua.edu.cn
+mirrorbits enable tsinghua.edu.cn
+
+mirrorbits add -http=https://ftp.acc.umu.se/mirror/msys2.org/ -rsync=rsync://ftp.acc.umu.se/mirror/msys2.org/ -score 1000 umu.se
+mirrorbits enable umu.se
+
+mirrorbits add -http=https://quantum-mirror.hu/mirrors/pub/msys2/ -rsync=rsync://quantum-mirror.hu/msys2/ -score 1000 quantum-mirror.hu
+mirrorbits enable quantum-mirror.hu
+
+mirrorbits add -http=https://mirrors.dotsrc.org/msys2/ -rsync=rsync://mirrors.dotsrc.org/msys2/ -score 1000 dotsrc.org
+mirrorbits enable dotsrc.org
+
+mirrorbits add -http=https://ftp.cc.uoc.gr/mirrors/msys2/ -ftp=ftp://ftp.cc.uoc.gr/mirrors/msys2/ -score 1000 uoc.gr
+mirrorbits enable uoc.gr
---
+++ mirrorbits/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+while [ ! -f /usr/share/GeoIP/GeoLite2-ASN.mmdb ] || [ ! -f /usr/share/GeoIP/GeoLite2-ASN.mmdb ]
+do
+    echo "waiting for GeoIP data to be loaded"
+    sleep 2
+done
+
+mirrorbits daemon --debug &
+pid=$!
+
+while ! mirrorbits list
+do
+  echo "waiting for server"
+  sleep 3
+done
+
+# remove all mirrors
+mirrorbits list -state=false | tail -n +2 | xargs --no-run-if-empty -n1 mirrorbits remove -f
+
+# add all mirrors
+if [ -f "add_mirrors.sh" ]; then
+    source add_mirrors.sh
+fi
+
+wait $pid
